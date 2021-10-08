75496809a2dfa1bdc0fc2ebfbeea906ac53525d1
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-08 18:52:47 -0500

    Added create and update methods to Customer Container.

--- src/Components/Customergroup/CustomergroupContainer.js
+++ src/Components/Customergroup/CustomergroupContainer.js
@@ -22,4 +22,70 @@ useEffect(() => {
       })
   }, []);
 
+  function populateCustomergroup() {
+    console.log(customergroups);
+    return customergroups.map((customergroup, idx) => (
+      <Customergroup customergroup={customergroup} updateCustomergroup={updateCustomergroup} deleteCustomergroup={deleteCustomergroup} key={customergroup.id} />
+    ));
+  }
+  function createCustomergroup(customergroup) {
+    fetch(BASE_URL + "Customergroup", {
+      method: "POST",
+      body: JSON.stringify(customergroup),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => setCustomergroup([...customergroups, json]));
+
+  }
+
+  //  UPDATE
+       
+  function updateCustomergroup(customergroup) {
+    fetch(BASE_URL + "Customergroups/" + customergroup.id, {
+        method: "PUT",
+        body: JSON.stringify(customergroup),
+        headers: {
+       "Accept": "applicaton/json",
+       "Content-Type": "application/json",
+       },
+    });
+    function createCustomergroup(customergroup) {
+        fetch(BASE_URL + "customergroup", {
+          method: "POST",
+          body: JSON.stringify(customergroup),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((json) => setCustomergroup([...customergroup, json]));
+    
+      }
+    
+      //  UPDATE
+           
+      function updateCustomergroup(customergroup) {
+        fetch(BASE_URL + "customergroups/" + customergroup.id, {
+            method: "PUT",
+            body: JSON.stringify(customergroup),
+            headers: {
+           "Accept": "applicaton/json",
+           "Content-Type": "application/json",
+           },
+        });
+
+        const newCustomergroup = customergroups.map ((c) => {
+            if (c.id === customergroup.id) {
+                c = fcustomergroup;
+            }
+            
+            return c;
+        });
+        setCustomergroups(newCustomergroups);
+        }
   export default CuctomergroupContainer;
